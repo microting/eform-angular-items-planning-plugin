752ffabc622e999e1546364f2ecb545e895e8f9a
Gid <mkazli733@gmail.com>
2020-10-08 11:36:07 +0300

    Added missing guards

--- eform-client/src/app/plugins/modules/items-planning-pn/enums/items-planning-pn-claims.const.ts
+++ eform-client/src/app/plugins/modules/items-planning-pn/enums/items-planning-pn-claims.const.ts
@@ -1,4 +1,6 @@
 export const ItemsPlanningPnClaims = {
   accessItemsPlanningPlugin: 'items_planning_plugin_access',
-  createPlannings: 'plannings_create'
+  createPlannings: 'plannings_create',
+  getPlannings: 'plannings_get',
+  editPlanning: 'planning_edit'
 };
