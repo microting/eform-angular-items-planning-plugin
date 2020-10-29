d88e4a677a5bc5bbf34dbf957b8d4695705cfaa4
oleksiygarnik <oleksiy.garnik27@gmail.com>
2020-10-29 12:43:26 +0200

    Implemented method to get navigation menu

---
+++ eFormAPI/Plugins/ItemsPlanning.Pn/ItemsPlanning.Pn/Infrastructure/Consts/LanguageNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemsPlanning.Pn.Infrastructure.Consts
+{
+    public static class LanguageNames
+    {
+        public const string English = "English";
+        public const string Danish = "Danish";
+        public const string German = "German";
+    }
+}
