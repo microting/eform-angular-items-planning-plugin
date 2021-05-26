1b70821dd814b91f6a64573044e3365ea3ad3e20
Alex <sahok6264@gmail.com>
2021-05-26 14:37:51 +0300

    Add sticky headers table

--- eform-client/src/app/plugins/modules/items-planning-pn/layouts/items-planning-pn-layout.component.ts
+++ eform-client/src/app/plugins/modules/items-planning-pn/layouts/items-planning-pn-layout.component.ts
@@ -1,7 +1,7 @@
-import {AfterContentInit, Component, OnInit,} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
-import {translates} from './../i18n/translates';
-import {AuthStateService} from 'src/app/common/store';
+import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { translates } from './../i18n/translates';
+import { AuthStateService } from 'src/app/common/store';
 
 @Component({
   selector: 'app-items-planning-pn-layout',
