2ec5e9b96f1538a2a356bb904bc935285ed889a5
Alex <sahok6264@gmail.com>
2022-03-08 17:01:52 +0200

    Adding image reset entire case

--- eform-client/src/app/plugins/modules/items-planning-pn/components/planning-case/planning-case-page/planning-case-page.component.ts
+++ eform-client/src/app/plugins/modules/items-planning-pn/components/planning-case/planning-case-page/planning-case-page.component.ts
@@ -11,12 +11,13 @@ import {
   TemplateDto,
   CaseEditRequest,
   ReplyElementDto,
-  ReplyRequest,
+  ReplyRequest, ElementDto, DataItemDto,
 } from 'src/app/common/models';
 import { AuthStateService } from 'src/app/common/store';
 import { CaseEditElementComponent } from 'src/app/common/modules/eform-cases/components';
 import {ItemsPlanningPnCasesService} from 'src/app/plugins/modules/items-planning-pn/services';
 import {DateTimeAdapter} from '@danielmoncada/angular-datetime-picker';
+import * as R from 'ramda';
 
 @Component({
   selector: 'app-installation-case-page',
@@ -123,5 +124,53 @@ export class PlanningCasePageComponent implements OnInit {
     setTimeout(() => {
       document.querySelector(location).parentElement.scrollIntoView();
     });
+  }
+
+  partialLoadCase() {
+    if (!this.id || this.id === 0) {
+      return;
+    }
+    this.casesService
+      .getById(this.id, this.currenteForm.id)
+      .subscribe((operation) => {
+        if (operation && operation.success) {
+          const fn = (pathForLens: Array<number | string>) => {
+            const lens = R.lensPath(pathForLens);
+            let dataItem: (ElementDto | DataItemDto) = R.view(lens, operation.model);
+            // @ts-ignore
+            if (dataItem.elementList !== undefined || dataItem.dataItemList !== undefined) {
+              dataItem = dataItem as ElementDto;
+              // R.set(R.lensPath([...pathForLens, 'extraPictures']), dataItem.extraPictures, this.replyElement);
+              if(dataItem.elementList) {
+                for (let i = 0; i < dataItem.elementList.length; i++) {
+                  fn([...pathForLens, 'elementList', i]);
+                }
+              }
+              if(dataItem.dataItemList) {
+                for (let i = 0; i < dataItem.dataItemList.length; i++) {
+                  fn([...pathForLens, 'dataItemList', i]);
+                }
+              }
+            } else { // @ts-ignore
+              if(dataItem.fieldType !== undefined){
+                dataItem = dataItem as DataItemDto;
+                if (dataItem.fieldType === 'FieldContainer') {
+                  for (let i = 0; i < dataItem.dataItemList.length; i++) {
+                    fn([...pathForLens, 'dataItemList', i]);
+                  }
+                }
+                if (dataItem.fieldType === 'Picture') {
+                  // let oldDataItem = R.view(lens, this.replyElement);
+                  // oldDataItem = {...oldDataItem, ...dataItem};
+                  this.replyElement = R.set(lens, dataItem, this.replyElement);
+                }
+              }
+            }
+          }
+          for (let i = 0; i < operation.model.elementList.length; i++){
+            fn(['elementList', i]);
+          }
+        }
+      });
   }
 }
