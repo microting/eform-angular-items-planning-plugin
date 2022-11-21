e970a7483380e234dbb039962e903d77d801dfb5
René Schultz Madsen <rm@microting.com>
2022-11-21 14:04:21 +0000

    Fixing plugin settings page.

--- eform-client/e2e/Tests/items-planning-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/items-planning-settings/application-settings.plugins-page.spec.ts
@@ -11,13 +11,13 @@ describe('Application settings page - site header section', function () {
   it('should go to plugin settings page', async () => {
     await loginPage.login();
     await myEformsPage.Navbar.goToPluginsPage();
-    await (await $('#plugin-name')).waitForDisplayed({ timeout: 50000 });
+    //await (await $('#plugin-name')).waitForDisplayed({ timeout: 50000 });
 
     const plugin = await pluginPage.getFirstPluginRowObj();
     expect(plugin.id).equal(1);
     expect(plugin.name).equal('Microting Items Planning Plugin');
     expect(plugin.version).equal('1.0.0.0');
-    expect(plugin.status, 'status is not equal').eq(false);
+    expect(plugin.status, 'status is not equal').eq('toggle_off');
   });
 
   it('should activate the plugin', async () => {
@@ -29,6 +29,6 @@ describe('Application settings page - site header section', function () {
     expect(plugin.id).equal(1);
     expect(plugin.name).equal('Microting Items Planning Plugin');
     expect(plugin.version).equal('1.0.0.0');
-    expect(plugin.status, 'status is not equal').eq(true);
+    expect(plugin.status, 'status is not equal').eq('toggle_on');
   });
 });
